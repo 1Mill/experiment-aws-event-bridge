6ed2c6183247571d56d8f23897eaefb1c281d1e5
1Mill <erik.ekberg@outlook.com>
2023-08-15 14:10:45 -0700

    Create aws_lambda_permission

--- src/event-bridge/main.tf
+++ src/event-bridge/main.tf
@@ -10,3 +10,10 @@ resource "aws_cloudwatch_event_target" "this" {
 	arn  = var.lambda_arn
 	rule = resource.aws_cloudwatch_event_rule.this.name
 }
+
+resource "aws_lambda_permission" "this" {
+	action        = "lambda:InvokeFunction"
+	function_name = var.lambda_arn
+	principal     = "events.amazonaws.com"
+	source_arn    = resource.aws_cloudwatch_event_rule.this.arn
+}
