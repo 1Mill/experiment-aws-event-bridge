52cfe03c8609e98a2ceb55453b0bbe46928b302a
1Mill <erik.ekberg@outlook.com>
2023-08-17 10:18:01 -0700

    Scaffold tasks

--- src/event-bridge/main.tf
+++ src/event-bridge/main.tf
@@ -1,19 +1,25 @@
 variable "lambda_arn" { type = string }
 
-resource "aws_cloudwatch_event_rule" "this" {
-	description         = "retry scheuled every 1 minute"
-	name                = "profile-generator-lambda-event-rule"
-	schedule_expression = "rate(1 minute)"
-}
+# ! Part 1 - Create a rule on the "default" Event Bridge to invoke
+# ! the input Lambda once every minute using the native resources
+# ! provided by the AWS provider.
+# resource "aws_cloudwatch_event_rule" "this" {
+# 	description         = "retry scheuled every 1 minute"
+# 	name                = "profile-generator-lambda-event-rule"
+# 	schedule_expression = "rate(1 minute)"
+# }
 
-resource "aws_cloudwatch_event_target" "this" {
-	arn  = var.lambda_arn
-	rule = resource.aws_cloudwatch_event_rule.this.name
-}
+# resource "aws_cloudwatch_event_target" "this" {
+# 	arn  = var.lambda_arn
+# 	rule = resource.aws_cloudwatch_event_rule.this.name
+# }
 
-resource "aws_lambda_permission" "this" {
-	action        = "lambda:InvokeFunction"
-	function_name = var.lambda_arn
-	principal     = "events.amazonaws.com"
-	source_arn    = resource.aws_cloudwatch_event_rule.this.arn
-}
+# resource "aws_lambda_permission" "this" {
+# 	action        = "lambda:InvokeFunction"
+# 	function_name = var.lambda_arn
+# 	principal     = "events.amazonaws.com"
+# 	source_arn    = resource.aws_cloudwatch_event_rule.this.arn
+# }
+
+# ! Part 2 - Do the same as Part 1 but use AWS Terraform Modules.
+
