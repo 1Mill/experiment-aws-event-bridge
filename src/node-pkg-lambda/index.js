c3f0b8266cecda161ea48f6f988eb756922ee188
1Mill <erik.ekberg@outlook.com>
2023-09-10 19:56:51 -0700

    Invoke handler

--- src/node-pkg-lambda/index.js
+++ src/node-pkg-lambda/index.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch')
+const { faker } = require('@faker-js/faker')
 
 // * To create a custom runtime for AWS, lambda, we fetch our event from and return our payload to
 // * the base url that they provide us. https://docs.aws.amazon.com/lambda/latest/dg/runtimes-api.html
 const BASE_URL = `http://${process.env.AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation`
 
-const main = async () => {
+const main = async (handler) => {
 	while (true) {
 		// * Fetch event from AWS
 		const invokeResponse = await fetch(`${BASE_URL}/next`)
@@ -16,12 +17,12 @@ const main = async () => {
 		const event = await invokeResponse.json()
 
 		// * Generate our results
-		const result = 'aaaaa'
+		const result = await handler(event, { todo: true }) // TODO: Pass through AWS Lambda context
 
 		// * Return our results to any request waiting for our results (i.e. invocationType: "RequestResponse")
 		const awsRequestId = invokeResponse.headers.get('Lambda-Runtime-Aws-Request-Id')
 		const returnResponse = await fetch(`${BASE_URL}/${awsRequestId}/response`, {
-			body: JSON.stringify({ event, result }),
+			body: JSON.stringify({ result }),
 			method: 'POST',
 		})
 		if (returnResponse.status !== 202) {
@@ -30,25 +31,23 @@ const main = async () => {
 	}
 }
 
-main()
+const handler = async (cloudevent = {}, ctx = {}) => {
+	console.log('starting')
 
-// const { faker } = require('@faker-js/faker');
+	const profile = {
+		firstName: faker.person.firstName(),
+		lastName: faker.person.lastName(),
+		phoneNumber: faker.phone.number(),
+		vehicleType: faker.vehicle.model(),
+	}
 
-// exports.handler = async (cloudevent = {}, ctx = {}) => {
-// 	console.log('starting')
+	console.log('profile generated')
 
-// 	const profile = {
-// 		firstName: faker.person.firstName(),
-// 		lastName: faker.person.lastName(),
-// 		phoneNumber: faker.phone.number(),
-// 		vehicleType: faker.vehicle.model(),
-// 	}
+	return {
+		body: JSON.stringify({ profile }),
+		headers: { 'Content-Type': 'application/json' },
+		statusCode: 200,
+	}
+};
 
-// 	console.log('profile generated')
-
-// 	return {
-// 		body: JSON.stringify({ profile }),
-// 		headers: { 'Content-Type': 'application/json' },
-// 		statusCode: 200,
-// 	}
-// };
+main(handler)
