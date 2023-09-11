44a8204a640bac0be41904ba0d3bd5d762f92902
1Mill <erik.ekberg@outlook.com>
2023-09-11 09:22:19 -0700

    Reduce timeout

--- src/node-pkg-lambda/main.tf
+++ src/node-pkg-lambda/main.tf
@@ -6,7 +6,7 @@ module "lambda" {
 	environment = { NODE_ENV = "production" }
 	function    = {
 		name    = "node-pgk-lambda"
-		timeout = 7
+		timeout = 3
 		# version = "some-static-value-to-avoid-docker-rebuilds"
 	}
 }
